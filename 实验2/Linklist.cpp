64f8d0a63c6e823e86cd1d27d7c25a15e0727124
zyg0121 <zhouyiguo2012@qq.com>
2020-10-03 11:19:39 +0800

    实验2仅完成函数部分

--- 实验2/Linklist.cpp
+++ 实验2/Linklist.cpp
@@ -47,244 +47,84 @@ public:
 
 };
 
-const int LIST_INIT_SIZE = 100;
-const int LISTINCRMENT = 20;
+typedef struct LNode {
+	stu_dorm data;
+	struct LNode *next;
+}LNode,*LinkList;
 
-typedef struct {
-	stu_dorm *elem;
-	int length;
-	int listsize;
-}SqList;
-
-int IntiList_Sq(SqList &L) {
-	L.elem = (stu_dorm *)malloc(LIST_INIT_SIZE * sizeof (stu_dorm));
-	if (!L.elem) exit(-2);
-	L.length = 0;
-	L.listsize = LIST_INIT_SIZE;
-	return 1;
+bool GetEmem_L(LinkList L,int i,stu_dorm &e) {
+	LNode *p = L->next;
+	int j = 1;
+	while(p && j<i) {
+		p =  p->next;
+		++j;
+	}
+	if(!p || j>i) return false;
+	e = p->data;
+	return true;
 }
 
-int Input_Sq(SqList &L, int n) {
-	stu_dorm *newbase;
-	if (n < 0) return false;
-	if (n > L.listsize) {
-		newbase = (stu_dorm *)malloc(LIST_INIT_SIZE * sizeof (stu_dorm));
-		if (!newbase)
-			exit(-2);
-		L.elem = newbase;
-		L.listsize += LISTINCRMENT;
+bool ListInsert_L(LinkList &L,int i,stu_dorm e) {
+	LNode *p = L;
+	int j = 0;
+	while(p && j<i-1) {
+		p = p->next;
+		++j;
 	}
-	printf("Please enter the dorm element(num,size,people,condition(1 or 0):\n");
+	if(!p || j > i-1) return false;
+	LNode *s = (LinkList)malloc(sizeof(LNode));
+	s->data = e;
+	s->next = p->next;
+	p->next = s;
+	return true;
+}
+
+bool ListDelete_L(LinkList &L,int i,stu_dorm &e) {
+	LNode *p = L;
+	int j = 0;
+	while(p->next && j < i-1) {
+		p = p->next;
+		++j;
+	}
+	if(!(p->next)||j > i-1) return false;
+	LNode *q = p->next;
+	p->next = q->next;
+	e = q->data;
+	free(q);
+	return true;
+}
+
+void CreateList_L(LinkList &L,int n) {
+	L = (LinkList)malloc(sizeof(LNode));
+	L->next = NULL;
 	int num, people;
 	double size;
 	int condition;
-	for (int i = 0; i < n; i++) {
+	for(int i = n;i > 0;i--) {
+		LNode *p = (LinkList) malloc(sizeof(LNode));
 		cin>>num;
 		cin>>size;
 		cin>>people;
 		cin>>condition;
-		L.elem[i].set_condition(condition);
-		L.elem[i].set_num(num);
-		L.elem[i].set_pople(people);
-		L.elem[i].set_size(size);
-		L.length++; 
+		L->data.set_condition(condition);
+		L->data.set_num(num);
+		L->data.set_pople(people);
+		L->data.set_size(size);
+		p->next = L->next;
+		L->next = p;
 	}
-	return true;
 }
 
-int ListInsert_Sq(SqList &L, int i, stu_dorm e) {
-	stu_dorm *newbase;
-	if (i<1 || i>L.length + 1) return false;
-	if (L.length >= L.listsize) {
-		newbase = (stu_dorm *)realloc(L.elem, (L.listsize + LISTINCRMENT) * sizeof(stu_dorm));
-		if (!newbase) exit(0);
-		L.elem = newbase;
-		L.listsize += LISTINCRMENT;
+void MergeList_L(LinkList &La,LinkList &Lb,LinkList &Lc) {
+	LNode *pa = La->next;
+	LNode *pb = Lb->next;
+	LNode *pc;
+	Lc = pc = La;
+	while(pa) {
+		pc->next = pa; pc = pa; pa = pa->next;
 	}
-	stu_dorm *q = &(L.elem[i - 1]);
-	for (stu_dorm *p = &(L.elem[L.length - 1]); p >= q; --p) *(p + 1) = *p;
-	*q = e;
-	++L.length;
-	return true;
-}
-
-int ListDelete_Sq(SqList &L, int i, stu_dorm &e) {
-	if (i<1 || i>L.length) return false;
-	stu_dorm *p = &(L.elem[i - 1]);
-	e = *p;
-	stu_dorm *q = L.elem + L.length - 1;
-	for (++p; p <= q; ++p) *(p - 1) = *p;
-	--L.length;
-	return true;
-}
-
-int LocateElem_Sq(SqList L, int e, int(*compare)(stu_dorm, int)) {
-	int i = 1;
-	stu_dorm *p = L.elem;
-	while (i <= L.length && !(*compare)(*p++, e)) ++i;
-	if (i <= L.length) return i;
-	else return 0;
-}
-
-int compare(stu_dorm c1,int c2){
-	if(c1.get_num()==c2)
-		return 1;
-	else
-		return 0;
-}
-
-void MergeList_Sq(SqList La, SqList Lb, SqList &Lc) {
-	stu_dorm *pa = La.elem,*pb = Lb.elem;
-	Lc.listsize = Lc.length = La.length + Lb.length;
-	stu_dorm *pc = Lc.elem = (stu_dorm*)malloc(Lc.listsize * sizeof(stu_dorm));
-	if (!Lc.elem) exit(0);
-	stu_dorm *pa_last = La.elem + La.length - 1;
-	stu_dorm *pb_last = Lb.elem + Lb.length - 1;
-	while (pa <= pa_last) *pc++ = *pa++;
-	while (pb <= pb_last) *pc++ = *pb++;
-}
-
-int Output_Sq(SqList &L) {
-	stu_dorm *p = L.elem;
-	stu_dorm *p_last = L.elem + L.length - 1;
-	if (!L.elem) exit(0);
-	printf("SqList(num,size,people,condition):\n");
-	for(int i=0;i<L.length;i++)
-		cout<<L.elem[i].get_num()<<' '<<L.elem[i].get_size()<<' '<<L.elem[i].get_people()<<' '<<L.elem[i].get_condition()<<endl;
-	return true;
-}
-
-void menu1() {
-	puts("1.insert");
-	puts("2.delete");
-	puts("3.find");
-	puts("4.print");
-	puts("5.Merge");
-	puts("0.exit");
-}
-
-void menu2() {
-	puts("1.first_floor");
-	puts("2.second_floor");
-}
-
-int main() {
-	SqList first_floor;
-	SqList second_floor;
-	puts("Start initialization...");
-	puts("Plese enter the first floor dorm element:");
-	IntiList_Sq(first_floor);
-	int k;
-	puts("please enter the number of the rooms");
-	cin>>k;
-	Input_Sq(first_floor, k);
-	puts("Plese enter the second floor dorm element:");
-	IntiList_Sq(second_floor);
-	puts("please enter the number of the rooms");
-	cin>>k;
-	Input_Sq(second_floor, k);
-	
-	puts("Please enter the order:");
-	int order1,order2;
-	menu1();
-	cin>>order1;
-	while(order1!=0) {
-		if (order1 >= 1 && order1 <= 4) {
-			puts("Please enter the floor");
-			cin>>order2;
-			if (order2 == 1) {
-				if (order1 == 1) {
-					int i;
-					puts("Please enter the pos do you want insert:");
-					cin>>i;
-					int num, people;
-					double size;
-					int condition;
-					printf("Please enter the dorm element(num,size,people,condition(1 or 0):\n");
-					cin >> num;
-					cin >> size;
-					cin >> people;
-					cin >> condition;
-					stu_dorm e(num, size, people, condition);
-					ListInsert_Sq(first_floor, i, e);
-					Output_Sq(first_floor);
-				}
-				else if (order1 == 2) {
-					int i;
-					puts("Please enter the pos do you want delete:");
-					cin >> i;
-					ListDelete_Sq(first_floor, i, first_floor.elem[i]);
-					Output_Sq(first_floor);
-				}
-				else if (order1 == 3) {
-					int i;
-					puts("Please enter the room number do you want to find");
-					cin>>i;
-					cout<<"The pos is"<<LocateElem_Sq(first_floor,i,compare);
-				}
-				else if (order1 == 4) {
-					Output_Sq(first_floor);
-				}
-				else {
-					puts("ERROR input");
-				}
-			}
-			else if(order2==2) {
-				if (order1 == 1) {
-					int i;
-					puts("Please enter the pos do you want insert:");
-					cin>>i;
-					int num, people;
-					double size;
-					int condition;
-					printf("Please enter the dorm element(num,size,people,condition(1 or 0):\n");
-					cin >> num;
-					cin >> size;
-					cin >> people;
-					cin >> condition;
-					stu_dorm e(num, size, people, condition);
-					ListInsert_Sq(second_floor, i, e);
-					Output_Sq(second_floor);
-				}
-				else if (order1 == 2) {
-					int i;
-					puts("Please enter the pos do you want delete:");
-					cin >> i;
-					ListDelete_Sq(second_floor, i, second_floor.elem[i]);
-					Output_Sq(second_floor);
-				}
-				else if (order1 == 3) {
-					int i;
-					puts("Please enter the room number do you want to find");
-					cin>>i;
-					cout<<"The pos is"<<LocateElem_Sq(second_floor,i,compare);
-				}
-				else if (order1 == 4) {
-					Output_Sq(second_floor);
-				}
-				else {
-					puts("ERROR input");
-				}
-			}
-			else {
-				puts("ERROR input");
-			}
-		}
-		else if (order1 == 5) {
-			SqList all;
-			IntiList_Sq(all);
-			MergeList_Sq(first_floor, second_floor, all);
-			Output_Sq(all);
-		}
-		else {
-			puts("ERROR input");
-		}
-		putchar(10);
-		puts("-----------------------------");
-		puts("Please enter the order:");
-		menu1();
-		cin>>order1;
+	while(pb) {
+		pc->next = pb; pc = pb; pb = pb->next;
 	}
-	
-	return 0;
+	free(Lb);
 }
