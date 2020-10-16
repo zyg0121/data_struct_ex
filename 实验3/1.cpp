0e7d86e25b3aef30cbf9d0558cf3b2e70196631c
zyg0121 <zhouyiguo2012@qq.com>
2020-10-16 14:50:37 +0800

    实验3-1解决

--- 实验3/1.cpp
+++ 实验3/1.cpp
@@ -1,12 +1,11 @@
 #include <cstdio>
 #include <cstring>
 #include <iostream>
-#include <string>
 #include <cstdlib>
 using namespace std;
 
 typedef struct QNode {
-    string name;
+    long long account;
     struct QNode *next;
 }QNode, *QueuePtr;
 
@@ -15,10 +14,18 @@ typedef struct {
     QueuePtr rear;
 }LinkQueue;
 
-bool InitQueue(LinkQueue &Q) {
-    Q.front = Q.rear = (QueuePtr)malloc(sizeof(QNode));
-    if(!Q.front) exit(-2);
-    Q.front->next = NULL;
+bool InitQueue(LinkQueue *Q) {
+    if(Q == NULL) {
+        return false;
+    }
+    
+    Q->front = Q->rear = (QueuePtr) malloc(sizeof(QNode));
+    if(!Q->front) {
+        exit(-2);
+    }
+    
+    Q->front->next = NULL;
+    
     return true;
 }
 
@@ -49,36 +56,37 @@ bool DestroyQueue(LinkQueue &Q) {
     return true;
 }
 
-bool EnQueue(LinkQueue &Q,string &e) {
+bool EnQueue(LinkQueue &Q,long long &e) {
     QueuePtr p = (QueuePtr)malloc(sizeof(QNode));
     if(!p) exit(-2);
-    p->name = e;
+    p->account = e;
     p->next = NULL;
     Q.rear->next = p;
     Q.rear = p;
     return true;
 }
 
-bool DeQueue(LinkQueue &Q,string &e) {
+bool DeQueue(LinkQueue &Q,long long &e) {
     if(Q.front == Q.rear) return false;
     QueuePtr p = Q.front->next;
-    e = p->name;
+    e = p->account;
     Q.front->next = p->next;
     if(Q.rear==p) Q.rear = Q.front;
     free(p);
     return true;
 }
 
-bool GetHead(LinkQueue Q, string& e) {
+bool GetHead(LinkQueue &Q, long long& e) {
     QueuePtr p;
     if(Q.front == NULL || Q.front == Q.rear) {
         return false;
     }
     p = Q.front->next;
-    e = p->name;
+    e = p->account;
     
     return true;
 }
+
 
 void menu() {
     puts("请进行操作");
@@ -89,29 +97,29 @@ void menu() {
 
 int main() {
     LinkQueue bank;
-    InitQueue(bank);
+    InitQueue(&bank);
     menu();
     int i;
-    string name,enname,dename;
+    long long account = 0;
     cin>>i;
     while(i) {
         printf("前方剩余%d人\n",QueueLength(bank));
-        GetHead(bank,name);
-        if(name=="") cout<<"无下一位"<<endl;
-        cout<<"下一位:"<<name<<endl;
+        GetHead(bank,account);
+        if(account==0) cout<<"无下一位"<<endl;
+        cout<<"下一位:"<<account<<endl;
         switch (i) {
-        case 1:
-            puts("请输入您的姓名：");
-            cin>>enname;
-            EnQueue(bank,enname);
-            printf("已经开始排队，前方剩余%d人\n",QueueLength(bank));
-            break;
-        case 2:
-            DeQueue(bank,dename);
-            cout<<"请"<<dename<<"到前台办理业务"<<endl;
-            break;
-        default:
-            break;
+            case 1:
+                puts("请输入您的卡号：");
+                cin>>account;
+                EnQueue(bank,account);
+                printf("已经开始排队，前方剩余%d人\n",QueueLength(bank));
+                break;
+            case 2:
+                DeQueue(bank,account);
+                cout<<"请"<<account<<"到前台办理业务"<<endl;
+                break;
+            default:
+                break;
         }
         menu();
         cin>>i;
